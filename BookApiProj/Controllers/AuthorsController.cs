545fce5e91b044af5dba5dc47b78791db3360180
Amir Ibragimov <ibragimov-amir@list.ru>
2021-11-22 00:21:53 +0600

    Authors GetByID GetBooksByAuthor GetAuthorsOfABook

--- BookApiProj/Controllers/AuthorsController.cs
+++ BookApiProj/Controllers/AuthorsController.cs
@@ -14,10 +14,12 @@ namespace BookApiProj.Controllers
     public class AuthorsController : ControllerBase
     {
         private IAuthorRepository _authorRepository;
+        private IBookRepository _bookRepository;
 
-        public AuthorsController(IAuthorRepository authorRepository)
+        public AuthorsController(IAuthorRepository authorRepository, IBookRepository bookRepository)
         {
             _authorRepository = authorRepository;
+            _bookRepository = bookRepository;
         }
 
         //api/authors
@@ -50,33 +52,107 @@ namespace BookApiProj.Controllers
         }
 
 
-        //api/authors/reviewerId
-        [HttpGet("{reviewerId}")]
+        //api/authors/authorId
+        [HttpGet("{authorId}")]
         [ProducesResponseType(400)]
         [ProducesResponseType(404)]
-        [ProducesResponseType(200, Type = typeof(IEnumerable<ReviewerDto>))]
-        public async Task<IActionResult> GetReviewer(int reviewerId)
+        [ProducesResponseType(200, Type = typeof(IEnumerable<AuthorDto>))]
+        public async Task<IActionResult> GetAuthor(int authorId)
         {
-            if (!_reviewerRepository.ReviewerExists(reviewerId))
+            if (!_authorRepository.AuthorExists(authorId))
             {
                 return NotFound();
             }
 
-            var reviewer = _reviewerRepository.GetReviewer(reviewerId);
+            var author = _authorRepository.GetAuthor(authorId);
 
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            var reviewerDto = new ReviewerDto
+            var authorDto = new AuthorDto
             {
-                Id = reviewer.Id,
-                FirstName = reviewer.FirstName,
-                LastName = reviewer.LastName
+                Id = author.Id,
+                FirstName = author.FirstName,
+                LastName = author.LastName
             };
 
-            return Ok(reviewerDto);
+            return Ok(authorDto);
+        }
+
+
+
+        //api/authors/authorId/books
+        [HttpGet("{authorId}/books")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<BookDto>))]
+        public async Task<IActionResult> GetBooksByAuthor(int authorId)
+        {
+            if (!_authorRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var books = _authorRepository.GetBooksByAuthor(authorId);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bookDto = new List<BookDto>();
+
+            foreach (var book in books)
+            {
+                bookDto.Add(new BookDto()
+                {
+                    Id = book.Id,
+                    Isbn = book.Isbn,
+                    Title = book.Title,
+                    DatePublished = book.DatePublished
+                });
+            }
+
+            return Ok(bookDto);
+        }
+
+
+
+
+        //api/authors/books/authorId
+        [HttpGet("books/{bookId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<AuthorDto>))]
+        public async Task<IActionResult> GetAuthorsOfABook(int bookId)
+        {
+            if (!_bookRepository.BookExists(bookId))
+            {
+                return NotFound();
+            }
+
+            var authors = _authorRepository.GetAuthorsOfABook(bookId);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var authorsDto = new List<AuthorDto>();
+
+            foreach (var author in authors)
+            {
+                authorsDto.Add(new AuthorDto
+                {
+                    Id = author.Id,
+                    FirstName = author.FirstName,
+                    LastName = author.LastName
+                });
+            }
+
+            return Ok(authorsDto);
         }
     }
 }
